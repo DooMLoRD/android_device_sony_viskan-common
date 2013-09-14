8c65abc5966be9a391171ef0abb2e8c218196e6c
DooMLoRD <metoo.mememe@gmail.com>
2013-09-15 03:07:40 +0530

    fix: kernel building args

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,8 +47,9 @@ TARGET_KRAIT_BIONIC_PLDSIZE   := 64
 # Kernel information
 BOARD_KERNEL_BASE     := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE  := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
-BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000
+SONY_FORCE_RAMDISK_ADDRESS := 0x81600000
+BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 androidboot.baseband=msm msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M
+BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x01500000
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
